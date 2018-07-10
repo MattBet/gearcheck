8689275000cc7e8eac8f99672de0be235241d3de
Mattis <mattis.betourne@gmail.com>
2018-07-10 13:15:58 +0200

    Pseudo element manipulation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,34 @@ const $ = require('jquery');
 // create global $ and jQuery variables
 global.$ = global.jQuery = $;
 
-$("document").ready(function () {
-    alert("Test jquery")
-})
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#backtotop').fadeIn();
+        } else {
+            $('#backtotop').fadeOut();
+        }
+    });
+
+    $('#backtotop').click(function() {
+        $('body,html').animate({scrollTop:0},400)
+    });
+});
+
+$(function() {
+    /**
+     * Smooth scrolling to page anchor on click
+     **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+            }
+        }
+    });
+});
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -16,18 +16,13 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, array(
-                'attr' => array('class' => 'form-control')
-            ))
-            ->add('username', TextType::class, array(
-                'attr' => array('class' => 'form-control')))
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password',
-                    'attr' => array('class' => 'form-control')),
-                'second_options' => array('label' => 'Repeat Password',
-                    'attr' => array('class' => 'form-control')),
-                ))
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
         ;
     }
 

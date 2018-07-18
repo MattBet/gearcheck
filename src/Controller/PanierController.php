6787d2a6ced65ea3f6d004c2b234b6be3681a46e
Mattis <mattis.betourne@gmail.com>
2018-07-18 18:40:39 +0200

    Cart logic completed

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\Product;
+use App\Entity\Shipping;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,10 +16,11 @@ class PanierController extends Controller
      */
     public function showAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
         //get current User
-        $user = $this->getUser();
+        $auth_checker = $this->get('security.authorization_checker');
+        $token = $this->get('security.token_storage')->getToken();
+        $state = $auth_checker->isGranted("IS_AUTHENTICATED_FULLY");
+        $user = $token->getUser();
 
         //Check if an user is logged in
         if(!$user)
@@ -24,8 +29,102 @@ class PanierController extends Controller
         }
 
 
+        $user_cart = $this->getDoctrine()->getRepository(Cart::class)->findBy(['user' => $user]);
+
+        if($user_cart)
+        {
+            $user_products = $this->getDoctrine()->getRepository(Shipping::class)->findBy(['cart' => $user_cart[0]->getId()]);
+
+            return $this->render('panier/index.html.twig', array(
+                'user_products' => $user_products,
+                'cart_data' => $user_cart[0],
+            ));
+        }
+
+
+
         return $this->render('panier/index.html.twig', array(
-            'user' => $user
+            'auth' => $auth_checker,
+            'user' => $user,
+            'user_cart' => $user_cart,
+            'state' => $state
         ));
+    }
+
+    /**
+     * @Route("/panier/add/{id}", name="add_to_cart")
+     */
+    public function addAction(Request $request, $id)
+    {
+        //Get user state
+        $auth_checker = $this->get('security.authorization_checker');
+        $user_logged = $auth_checker->isGranted("IS_AUTHENTICATED_FULLY");
+
+        if ($user_logged)
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            //Get user
+            $token = $this->get('security.token_storage')->getToken();
+            $user = $token->getUser();
+
+            //Get product id
+            $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+
+            //Check if user has cart
+            $exist_cart = $this->getDoctrine()->getRepository(Cart::class)->findBy(['user' => $user]);
+
+            if (!$exist_cart)
+            {
+                $cart = new Cart();
+
+                $cart->setUser($user);
+
+                $cart->setTotalPrice($product->getPrice());
+
+                $em->persist($cart);
+
+                $em->flush();
+
+                $ship = new Shipping();
+
+                $ship->setQuantity(1);
+
+                $ship->setProduct($product);
+
+                $ship->setCart($cart);
+
+                $em->persist($ship);
+
+                $em->flush();
+            }
+            else
+            {
+                $cart = $exist_cart[0];
+
+                $cart->setTotalPrice($cart->getTotalPrice() + $product->getPrice());
+
+                $em->persist($cart);
+
+                $em->flush();
+
+
+                $ship = new Shipping();
+
+                $ship->setQuantity(1);
+
+                $ship->setProduct($product);
+
+                $ship->setCart($cart);
+
+                $em->persist($ship);
+
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('panier');
+        }
+
+        return $this->redirectToRoute('home');
     }
 }
